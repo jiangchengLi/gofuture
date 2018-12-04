c062cb30acae2aeae3f2a267c8ea31457834da90
ljc <jiangcheng.li@sprixin.com>
2018-12-04 19:35:30 +0800

    搭建系统架构

---
+++ system-parent/system-auth/src/main/java/com/future/certification/Login.java
@@ -0,0 +1,16 @@
+package com.future.certification;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("login")
+public class Login {
+
+    @ResponseBody
+    @RequestMapping("getUserName")
+    public String getUserName(){
+        return "admin";
+    }
+}
